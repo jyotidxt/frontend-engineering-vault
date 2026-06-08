1b2761c22c3ad56c2da7184ba176d5f6e10dacc7
jyotidxt <dixit.jyoti004@gmail.com>
2026-06-08 21:17:27 +0530

    initial root directory added

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// /* <div className="p-3 bg-sky-500/10 text-sky-400 rounded-full mb-4 animate-bounce">
+//     ⚡
+//   </div> */
+  // src/App.jsx
+import React from 'react';
+// import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+// import HubHome from './HubHome';
+
+
+export default function App() {
+  return (
+    <div className="p-3 bg-sky-500/10 text-sky-400 rounded-full mb-4 animate-bounce">
+     ⚡
+      </div>
+   
+  );
+}
